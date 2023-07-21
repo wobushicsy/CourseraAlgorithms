5f767a54d178d6dafc0c3c43a545f44546f368c9
wobushicsy <csy657703828@163.com>
2023-07-21 20:38:29 +0800

    week2 Permutation done

--- part1/week2/Permutation.java
+++ part1/week2/Permutation.java
@@ -1,5 +1,17 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
 public class Permutation {
     public static void main(String[] args) {
-
+        int cnt = Integer.parseInt(args[0]);
+        RandomizedQueue<String> rq = new RandomizedQueue<>();
+        String s;
+        while (StdIn.isEmpty()) {
+            s = StdIn.readString();
+            rq.enqueue(s);
+        }
+        for (int i = 0; i < cnt; i += 1) {
+            StdOut.println(rq.dequeue());
+        }
     }
 }
