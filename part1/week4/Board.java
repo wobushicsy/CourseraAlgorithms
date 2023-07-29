93eb385122dcd9144bec501b2d5673af2ad4fd77
wobushicsy <csy657703828@163.com>
2023-07-29 22:24:47 +0800

    week4 Board.java done

--- part1/week4/Board.java
+++ part1/week4/Board.java
@@ -1,4 +1,7 @@
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
+
+import java.util.LinkedList;
 
 public class Board {
 
@@ -89,14 +92,89 @@ public class Board {
         return this.toString().equals(board1.toString());
     }
 
+    // Is the position on board?
+    private boolean inBoard(int i, int j) {
+        return i >= 0 && i < length && j >= 0 && j < length;
+    }
+
+    private int[][] copyBoard(int[][] board) {
+        int[][] tiles = new int[length][length];
+        for (int i = 0; i < length; i += 1) {
+            System.arraycopy(board[i], 0, tiles[i], 0, length);
+        }
+        return tiles;
+    }
+
+    private Board getNeighbor(int[][] board, int pos0I, int pos0J, int exI, int exJ) {
+        int[][] tiles = copyBoard(board);
+        int tmp = tiles[pos0I][pos0J];
+        tiles[pos0I][pos0I] = tiles[exI][exJ];
+        tiles[exI][exJ] = tmp;
+
+        return new Board(tiles);
+    }
+
     // all neighboring boards
     public Iterable<Board> neighbors() {
-        return null;
+        // return an array of integers to represent neighbors
+        // e.g. neighbor of (1, 1) is 1, 3, 5, 7
+        LinkedList<Board> neighbors = new LinkedList<>();
+
+        // get the position of 0
+        int posI = -1;
+        int posJ = -1;
+        for (int i = 0; i < length; i += 1) {
+            for (int j = 0; j < length; j += 1) {
+                if (board[i][j] != 0) {
+                    continue;
+                }
+                posI = i;
+                posJ = j;
+            }
+        }
+
+
+        // exchange with up
+        if (inBoard(posI + 1, posJ)) {
+            neighbors.add(getNeighbor(board, posI, posJ, posI + 1, posJ));
+        }
+        // exchange with down
+        if (inBoard(posI - 1, posJ)) {
+            neighbors.add(getNeighbor(board, posI, posJ, posI - 1, posJ));
+        }
+        // exchange with right
+        if (inBoard(posI, posJ + 1)) {
+            neighbors.add(getNeighbor(board, posI, posJ, posI, posJ + 1));
+        }
+        // exchange with left
+        if (inBoard(posI, posJ - 1)) {
+            neighbors.add(getNeighbor(board, posI, posJ, posI, posJ - 1));
+        }
+
+        return neighbors;
     }
 
     // a board that is obtained by exchanging any pair of tiles
     public Board twin() {
-        return null;
+        int[][] newBoard = copyBoard(board);
+        boolean changed = false;
+        for (int i = 0; i < length; i += 1) {
+            if (changed) {
+                break;
+            }
+            for (int j = 0; j < length - 1; j += 1) {
+                if (!(newBoard[i][j] > 0 && newBoard[i][j + 1] > 0)) {
+                    continue;
+                }
+                int tmp = newBoard[i][j];
+                newBoard[i][j] = newBoard[i][j + 1];
+                newBoard[i][j + 1] = tmp;
+                changed = true;
+                break;
+            }
+        }
+
+        return new Board(newBoard);
     }
 
     // unit testing (not graded)
@@ -107,6 +185,11 @@ public class Board {
         StdOut.println("toString() test: ");
         Board board1 = new Board(tiles);
         StdOut.println(board1.toString());
+
+        // dimension() test
+        StdOut.println("dimension() test: ");
+        StdOut.println(board1.dimension());
+        StdOut.println();
 
         // Hamming() test
         StdOut.println("Hamming() test: ");
@@ -128,7 +211,28 @@ public class Board {
         StdOut.println(board1.equals(board1));
         StdOut.println(board1.equals(new Board(tiles)));
         int[][] tiles1 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
-        StdOut.println(board1.equals(new Board(tiles1)));
+        Board board2 = new Board(tiles1);
+        StdOut.println(board1.equals(board2));
+        StdOut.println();
+
+        // neighbors() test
+        StdOut.println("neighbors() test: ");
+        StdOut.println("board1: \n" + board1);
+        for (Board neighbor: board1.neighbors()) {
+            StdOut.println(neighbor);
+        }
+//        StdOut.println("board2: \n" + board2);
+//        for (Board neighbor: board2.neighbors()) {
+//            StdOut.println(neighbor);
+//        }
+        StdOut.println();
+
+        // twin() test
+        StdOut.println("twin() test: ");
+        int[][] tiles2 = {{0, 2}, {1, 3}};
+        Board board3 = new Board(tiles2);
+        StdOut.println(board3.twin());
+        StdOut.println();
 
     }
 
