66e7bf49a7389d6242aa7823914e0b83886de770
wobushicsy <csy657703828@163.com>
2023-08-08 15:56:04 +0800

    week5 KdTree init

--- part1/week5/KdTree.java
+++ part1/week5/KdTree.java
@@ -1,2 +1,32 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
 public class KdTree {
+    public Iterable<Point2D> range(RectHV rect) {
+        return null;
+    }
+
+    public boolean contains(Point2D point) {
+        return false;
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public edu.princeton.cs.algs4.Point2D nearest(Point2D point) {
+        return null;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public void draw() {
+
+    }
+
+    public void insert(Point2D point2D) {
+
+    }
 }
