39292399e4d1268970df4c74b611dba6debae5d6
wobushicsy <csy657703828@163.com>
2023-08-14 22:27:42 +0800

    week2 doing

--- part2/week2/SeamCarver.java
+++ part2/week2/SeamCarver.java
@@ -1,30 +1,78 @@
 import edu.princeton.cs.algs4.Picture;
+import java.awt.Color;
 
 public class SeamCarver {
 
+    private Picture picture;
+    private int width;
+    private int height;
+
     // create a seam carver object based on the given picture
     public SeamCarver(Picture picture) {
+        checkNull(picture);
 
+        this.picture = new Picture(picture);
+        width = this.picture.width();
+        height = this.picture.height();
+    }
+
+    private void checkNull(Object obj) {
+        if (obj == null) {
+            throw new IllegalArgumentException("You can't call function by passing a null pointer");
+        }
+    }
+
+    private void checkIndices(int x, int y) {
+        if (x >= width || x < 0) {
+            throw new IllegalArgumentException("Index x is out of bound");
+        }
+        if (y >= height || y < 0) {
+            throw new IllegalArgumentException("Index y is out of bound");
+        }
     }
 
     // current picture
     public Picture picture() {
-        return null;
+        return picture;
     }
 
     // width of current picture
     public int width() {
-        return 0;
+        return width;
     }
 
     // height of current picture
     public int height() {
-        return 0;
+        return height;
+    }
+
+    private boolean checkBounder(int x, int y) {
+        return x == 0 || x == width - 1 || y == 0 || y == height - 1;
     }
 
     // energy of pixel at column x and row y
     public double energy(int x, int y) {
-        return 0;
+        checkIndices(x, y);
+
+        if (checkBounder(x, y)) {
+            return 1000;
+        }
+
+        Color left = picture.get(x - 1, y);
+        Color right = picture.get(x + 1, y);
+        int rx = left.getRed() - right.getRed();
+        int gx = left.getGreen() - right.getGreen();
+        int bx = left.getBlue() - right.getBlue();
+        int deltaXSquared = rx * rx + gx * gx + bx * bx;
+
+        Color up = picture.get(x - 1, y);
+        Color down = picture.get(x + 1, y);
+        int ry = up.getRed() - down.getRed();
+        int gy = up.getGreen() - down.getGreen();
+        int by = up.getBlue() - down.getBlue();
+        int deltaYSquared = ry * ry + gy * gy + by * by;
+
+        return Math.sqrt(deltaXSquared + deltaYSquared);
     }
 
     // sequence of indices for horizontal seam
@@ -39,12 +87,12 @@ public class SeamCarver {
 
     // remove horizontal seam from current picture
     public void removeHorizontalSeam(int[] seam) {
-
+        checkNull(seam);
     }
 
     // remove vertical seam from current picture
     public void removeVerticalSeam(int[] seam) {
-
+        checkNull(seam);
     }
 
     //  unit testing (optional)
