f14f308a52251cb0ba5d63afcc15df74217b0a51
wobushicsy <csy657703828@163.com>
2023-08-21 23:13:35 +0800

    week5 done

--- part2/week5/BurrowsWheeler.java
+++ part2/week5/BurrowsWheeler.java
@@ -1,21 +1,85 @@
+import edu.princeton.cs.algs4.BinaryStdIn;
+import edu.princeton.cs.algs4.BinaryStdOut;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class BurrowsWheeler {
 
     // apply Burrows-Wheeler transform,
     // reading from standard input and writing to standard output
     public static void transform() {
+        String inputString = BinaryStdIn.readString();
+        CircularSuffixArray circularSuffixArray = new CircularSuffixArray(inputString);
+        StringBuilder outputStringBuilder = new StringBuilder();
+        int length = circularSuffixArray.length();
 
+        for (int i = 0; i < length; i += 1) {
+            int index = circularSuffixArray.index(i);
+            if (index == 0) {
+                BinaryStdOut.write(i);
+            }
+            outputStringBuilder.append(inputString.charAt(index == 0 ? length - 1 : index - 1));
+        }
+        BinaryStdOut.write(outputStringBuilder.toString());
+
+        BinaryStdIn.close();
+        BinaryStdOut.close();
+    }
+
+    private static String sortString(String input) {
+        char[] tempArray = input.toCharArray();
+        Arrays.sort(tempArray);
+        return new String(tempArray);
     }
 
     // apply Burrows-Wheeler inverse transform,
     // reading from standard input and writing to standard output
     public static void inverseTransform() {
+        String inputString = BinaryStdIn.readString();
+        int first = Integer.parseInt(inputString.substring(0, 4));
+        String sortedSuffixLastCol = inputString.substring(4);
+        String sortedSuffixFirstCol = sortString(sortedSuffixLastCol);
+        int length = sortedSuffixLastCol.length();
 
+        int[] next = new int[length];
+        HashMap<Character, Integer> appearOnce = new HashMap<>();
+        for (int i = 0; i < length; i += 1) {
+            char c = sortedSuffixLastCol.charAt(i);
+            if (appearOnce.containsKey(c)) {
+                appearOnce.put(c, appearOnce.get(c) + 1);
+            } else {
+                appearOnce.put(c, 1);
+            }
+        }
+
+        for (char key : appearOnce.keySet()) {
+            if (appearOnce.get(key) != 1) {
+                continue;
+            }
+            int firstIndex = 0;
+            for (int i = 0; i < length; i += 1) {
+                if (sortedSuffixFirstCol.charAt(i) == key) {
+
+                }
+            }
+        }
+
+
+        BinaryStdIn.close();
+        BinaryStdOut.close();
     }
 
     // if args[0] is "-", apply Burrows-Wheeler transform
     // if args[0] is "+", apply Burrows-Wheeler inverse transform
     public static void main(String[] args) {
-
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Illegal command line argument");
+        }
+        if (args[0].equals("-"))            transform();
+        else if (args[0].equals("+"))       inverseTransform();
+        else throw new IllegalArgumentException("Illegal command line argument");
     }
 
 }
--- part2/week5/CircularSuffixArray.java
+++ part2/week5/CircularSuffixArray.java
@@ -1,23 +1,82 @@
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Arrays;
+
 public class CircularSuffixArray {
+
+    private final int length;
+    private final int[] index;
 
     // circular suffix array of s
     public CircularSuffixArray(String s) {
+        // corner case check
+        if (s == null) {
+            throw new IllegalArgumentException("Illegal command line argument");
+        }
+
+        // initialize variables
+        length = s.length();
+        index = new int[length];
+        if (length == 0) {
+            return;
+        }
+        StringBuilder stringBuilder = new StringBuilder(s);
+        String[] strings = new String[length];
+        strings[0] = s;
+        Str[] strs = new Str[length];
+        strs[0] = new Str(s, 0);
+
+        for (int i = 1; i < length; i += 1) {
+            char firstChar = stringBuilder.charAt(0);
+            stringBuilder.append(firstChar);
+            stringBuilder.deleteCharAt(0);
+            strings[i] = stringBuilder.toString();
+            strs[i] = new Str(strings[i], i);
+        }
+
+        Arrays.sort(strs);
+
+        for (int i = 0; i < length; i += 1) {
+            index[i] = strs[i].index;
+        }
+    }
+
+    private static class Str implements Comparable<Str> {
+        private final String string;
+        private final int index;
+
+        public Str(String s, int i) {
+            string = s;
+            index = i;
+        }
+
+        @Override
+        public int compareTo(Str o) {
+            return string.compareTo(o.string);
+        }
 
     }
 
     // length of s
     public int length() {
-        return 0;
+        return length;
     }
 
     // returns index of ith sorted suffix
     public int index(int i) {
-        return 0;
+        if (i < 0 || i >= length) {
+            throw new IllegalArgumentException("Index is out of bound");
+        }
+        return index[i];
     }
 
     // unit testing (required)
     public static void main(String[] args) {
-
+        String s = "ABRACADABRA!";
+        CircularSuffixArray circularSuffixArray = new CircularSuffixArray(s);
+        for (int i = 0; i < circularSuffixArray.length(); i += 1) {
+            StdOut.println(circularSuffixArray.index(i));
+        }
     }
 
 }
